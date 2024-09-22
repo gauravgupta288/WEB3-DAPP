86152d2d6b25f59140449e4278be5510989372a7
gauravgupta288 <guptagaurav288@gmail.com>
2024-09-22 20:05:24 +0530

    added send sol code

--- src/Components/SendTokens.jsx
+++ src/Components/SendTokens.jsx
@@ -1,7 +1,31 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import { useTransition } from "react";
+
 export function SendTokens(){
 
-    function sendToken(){
+    const wallet = useWallet()
+    const {connection} = useConnection();
+
+    async function sendToken(){
+        const publicKey = wallet.publicKey;
+        const to = document.getElementById('to').value;
+
+        const amount = document.getElementById('amount').value
+
+        const transaction = new Transaction();
         
+        transaction.add(SystemProgram.transfer({
+            fromPubkey : publicKey,
+            toPubkey : new PublicKey(to),
+
+            lamports : amount * LAMPORTS_PER_SOL
+        }));
+
+        await wallet.sendTransaction(transaction, connection)
+        alert("transfer successful")
+
+
     }
     return <div>
 
